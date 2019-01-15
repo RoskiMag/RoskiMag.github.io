c8f6f618bdfa2aa67f779481a4876d6297b0390e
Tim Malaney <tmalaney@usc.edu>
2019-01-14 22:14:12 -0800

    added react files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Submissions from './components/Submissions';
+import ViewSubmissions from './components/ViewSubmissions';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Nav />
+            <Route path="/" component={Home} exact />
+            <Route path="/submissions" component={Submissions} exact />
+            <Route path="/view-submissions" component={ViewSubmissions} exact />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
