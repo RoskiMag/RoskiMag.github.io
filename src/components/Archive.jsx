f9beefc31ce1e4d212e468924016f6c6aee42cbc
qianze <qianzezh@usc.edu>
2019-09-23 13:30:37 -0700

    remove mag 4 and application blob

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -16,7 +16,6 @@ export default class Archive extends Component {
       <div className="mag-container">
         <Link to="/archive/006"><a className="mag">006</a></Link>
         <Link to="/archive/005"><a className="mag">005</a></Link>
-        <Link to="/archive/004"><a className="mag">004</a></Link>
         <Link to="/archive/003"><a className="mag">003</a></Link>
         <Link to="/archive/002"><a className="mag">002</a></Link>
         <Link to="/archive/001"><a className="mag" onMouseOver={this.mouseOverLink()} onMouseOut={this.mouseOutLink()}>001</a></Link>
