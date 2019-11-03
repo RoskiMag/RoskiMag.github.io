67540f575a79bbeb5ad5374252d29f6e945d57d5
qianze <qianzezh@usc.edu>
2019-11-03 13:29:21 -0800

    hamburger nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom';
 import './css/Nav.css';
 
 import logo from './assets/RM_logo.png';
-import stickerMuleLogo from './assets/sticker_mule_logo.png';
 
 export default class Nav extends Component {
   render() {
     return (
-      <div className="nav-container">
+      <div className="header">
         <Link to="/">
         <img src={logo} alt="logo" className="logo" />
         </Link>
-        <Link to="/archive">
-        <p className="archive-link">MAGS</p>
-        </Link>
-        <Link to="/submissions">
-        <p className="submissions-link">SUBMISSIONS</p>
-        </Link>
-        <a href="https://roskimag.home.blog">
-        <p className="blog-link">BLOG</p>
-        </a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+        <ul class="menu">
+          <li><Link to="/archive">MAGS</Link></li>
+
+          <li><Link to="/submissions">SUBMISSIONS</Link></li>
+
+          <li><a href="https://roskimag.home.blog">BLOG</a></li>
+
+        </ul>
+
       </div>
     )
   }
