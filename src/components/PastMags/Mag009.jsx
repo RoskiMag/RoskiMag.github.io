2eafdf32e2610e39e555b8c3f7a5fa82b70cd132
Qianze Zhang <qianzezhang@Qianzes-MacBook-Pro.local>
2020-04-16 19:26:50 -0400

    open link in new tab

--- src/components/PastMags/Mag009.jsx
+++ src/components/PastMags/Mag009.jsx
@@ -8,14 +8,18 @@ export default class Mag009 extends Component {
         <span className="issue-header">ISSUE NINE</span>
         <span className="issue-theme">Love in the Time of the Internet</span>
           <div className="issue9-container-center">
-              <a href="https://drive.google.com/file/d/19AxXmVfQS6inX7H8DuIUVL-cQmaqduNF/view">
+              <a
+                target="_blank"
+                href="https://drive.google.com/file/d/19AxXmVfQS6inX7H8DuIUVL-cQmaqduNF/view">
               <img
                 src={require('../assets/issue_9/issue9_cover.png')}
                 className="cover"
                 />
                 </a>
                 <div className="view-button">
-                  <a href="https://drive.google.com/file/d/19AxXmVfQS6inX7H8DuIUVL-cQmaqduNF/view">
+                  <a
+                    target="_blank"
+                    href="https://drive.google.com/file/d/19AxXmVfQS6inX7H8DuIUVL-cQmaqduNF/view">
                   CLICK TO ENTER</a>
                 </div>
           </div>
