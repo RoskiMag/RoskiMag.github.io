24e139cbbdd1c69c7040bbe793dcee28ef898b22
RoskiMag <roskimag@gmail.com>
2021-04-27 15:08:17 -0700

    fixed chloe's error

--- src/components/PastMags/Mag011.jsx
+++ src/components/PastMags/Mag011.jsx
@@ -10,7 +10,7 @@ export default class Mag011 extends Component {
           <div className="issue9-container-center">
               <a
                 target="_blank"
-                href="https://drive.google.com/file/d/1YnZVL9yGuAXe9-hVTt7KF-2mTLOr84By/view?usp=sharing">
+                href="https://drive.google.com/file/d/1RJ33ZGiRCV544O9tyX0_RGutdXnm5ISV/view?usp=sharing">
               <img
                 src={require('../assets/issue_11/issue_11_cover.png')}
                 className="cover"
@@ -19,7 +19,7 @@ export default class Mag011 extends Component {
                 <div className="view-button">
                   <a
                     target="_blank"
-                    href="https://drive.google.com/file/d/1YnZVL9yGuAXe9-hVTt7KF-2mTLOr84By/view?usp=sharing">
+                    href="https://drive.google.com/file/d/1RJ33ZGiRCV544O9tyX0_RGutdXnm5ISV/view?usp=sharing">
                   VIEW ISSUE 11</a>
                 </div>
           </div>
