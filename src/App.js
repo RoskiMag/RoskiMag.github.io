5b428673272cc28da5e803510aa754b0b7982d02
RoskiMag <45381196+RoskiMag@users.noreply.github.com>
2023-12-16 10:13:20 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Archive from './components/Archive';
 import Blog from './components/Blog';
 import BlogPost from './components/BlogPost';
 
+import Mag016 from './components/PastMags/Mag016';
 import Mag015 from './components/PastMags/Mag015';
 import Mag014 from './components/PastMags/Mag014';
 import Mag013 from './components/PastMags/Mag013';
@@ -52,6 +53,7 @@ class App extends Component {
               <Route path="/archive" component={Archive} exact />
               <Route path="/blog" component={Blog} exact />
               <Route path="/blog/:postIndex" component={BlogPost} />
+              <Route path="/issue_16" component={Mag016} exact />
               <Route path="/issue_15" component={Mag015} exact />
               <Route path="/issue_14" component={Mag014} exact />
               <Route path="/issue_13" component={Mag013} exact />
