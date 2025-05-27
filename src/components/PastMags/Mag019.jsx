8d0d90c3e5e4cd6b830641378698300b5ee4233e
RoskiMag <45381196+RoskiMag@users.noreply.github.com>
2025-05-26 17:30:33 -0700

    Create Mag019.jsx

---
+++ src/components/PastMags/Mag019.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import './Mag.css';
+
+import img1 from '../assets/issue_19/001.jpg'
+import img2 from '../assets/issue_19/002.jpg'
+import img3 from '../assets/issue_19/003.jpg'
+import img4 from '../assets/issue_19/004.jpg'
+import img5 from '../assets/issue_19/005.jpg'
+import img6 from '../assets/issue_19/006.jpg'
+import img7 from '../assets/issue_19/007.jpg'
+import img8 from '../assets/issue_19/008.jpg'
+import img9 from '../assets/issue_19/009.jpg'
+import img10 from '../assets/issue_19/010.jpg'
+import img11 from '../assets/issue_19/011.jpg'
+import img12 from '../assets/issue_19/012.jpg'
+import img13 from '../assets/issue_19/013.jpg'
+import img14 from '../assets/issue_19/014.jpg'
+import img15 from '../assets/issue_19/015.jpg'
+import img16 from '../assets/issue_19/016.jpg'
+import img17 from '../assets/issue_19/017.jpg'
+import img18 from '../assets/issue_19/018.jpg'
+import img19 from '../assets/issue_19/019.jpg'
+import img20 from '../assets/issue_19/020.jpg'
+import img21 from '../assets/issue_19/021.jpg'
+import img22 from '../assets/issue_19/022.jpg'
+import img23 from '../assets/issue_19/023.jpg'
+
+
+
+export default class Mag015 extends Component {
+  render() {
+    return (
+      <div className="image-container">
+        <img src={img1} className="image"></img>
+        <img src={img2} className="image"></img>
+        <img src={img3} className="image"></img>
+        <img src={img4} className="image"></img>
+        <img src={img5} className="image"></img>
+        <img src={img6} className="image"></img>
+        <img src={img7} className="image"></img>
+        <img src={img8} className="image"></img>
+        <img src={img9} className="image"></img>
+        <img src={img10} className="image"></img>
+        <img src={img11} className="image"></img>
+        <img src={img12} className="image"></img>
+        <img src={img13} className="image"></img>
+        <img src={img14} className="image"></img>
+        <img src={img15} className="image"></img>
+        <img src={img16} className="image"></img>
+        <img src={img17} className="image"></img>
+        <img src={img18} className="image"></img>
+        <img src={img19} className="image"></img>
+        <img src={img20} className="image"></img>
+        <img src={img21} className="image"></img>
+        <img src={img22} className="image"></img>
+        <img src={img23} className="image"></img>
+
+        <img src={img30} className="image"></img>
+        <img src={img31} className="image"></img>
+      </div>
+    )
+  }
+}
