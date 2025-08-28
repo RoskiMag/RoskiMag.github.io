4a4a60d6d0f9d809b26a11ebc973609b604a5b46
RoskiMag <45381196+RoskiMag@users.noreply.github.com>
2025-08-27 20:31:58 -0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
 				</div>
 				
 				<div className="issue-button">
-					<a href="https://www.roskimag.com/#/issue_17">
-					READ ISSUE XVII
+					<a href="https://www.roskimag.com/#/issue_19">
+					READ ISSUE XIX
 					</a>
 				</div>
 				
