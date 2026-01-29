3f463d363b3563eb689088bac949fd61f597c347
RoskiMag <45381196+RoskiMag@users.noreply.github.com>
2026-01-29 12:59:09 -0800

    Update Archive.jsx

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -16,6 +16,10 @@ export default class Archive extends Component {
       <div className="mag-container">
 
         <div className="link-container">
+        <Link to="/issue_20"><a className="mag">20</a></Link>
+      </div>
+        
+        <div className="link-container">
         <Link to="/issue_19"><a className="mag">19</a></Link>
       </div>
         
