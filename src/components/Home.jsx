e3300277cb5134ee36f4a277d71a2082d37cf60c
RoskiMag <45381196+RoskiMag@users.noreply.github.com>
2026-08-25 09:05:29 -0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
 				</div>
 				
 				<div className="issue-button">
-					<a href="https://www.roskimag.com/#/issue_21">
-					READ ISSUE XXI
+					<a href="https://tripetto.app/run/UZ2ZRYSEFH">
+					APPLY NOW!
 					</a>
 				</div>
 				
